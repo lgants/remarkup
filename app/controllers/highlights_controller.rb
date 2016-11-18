76373147de569b901202404606e6606ce3ea3033
lgants <lgants@gmail.com>
2016-11-18 14:55:18 -0600

    completed methods to sort and merge data

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,38 +1,18 @@
 class HighlightsController < ApplicationController
-  before_action :set_user, only: [:show]
-  before_action :set_speech, only: [:show]
-  before_action :set_highlight, only: [:show]
+  before_action :set_user, only: [:show, :update]
+  before_action :set_speech, only: [:show, :destroy, :update]
+  before_action :set_highlight, only: [:show, :update]
+  before_action :set_snippet, only: [:update]
   # before_action :find, only: [:show, :create, :update, :destroy]
   # before action check_highlight used to check whether highlight alread exists
-
-
-  # GET /highlights
-  # GET /highlights.json
-  def index
-    @highlight = Highlight.all
-  end
 
   # GET /highlights/1
   # GET /highlights/1.json
   def show
-    # highlights = Highlight.find_by(user_id: @user_id, speech_id: @speech_id)
-
-    # @highlights = highlights
-    # @speech = Speech.find(speech_id)
-    # @user = User.find(user_id)
-    # @current_user = current_user
-
     respond_to do |format|
       format.js {}
     end
   end
-
-
-  # GET /highlights/1/edit
-  def edit
-  end
-
-
 
   # POST /highlights
   # POST /highlights.json
@@ -64,19 +44,20 @@ class HighlightsController < ApplicationController
   # PATCH/PUT /highlights/1
   # PATCH/PUT /highlights/1.json
   def update
-    speech_id = params[:data][:speech_id].to_i
-    user_id = current_user.id
-    snippets = JSON.parse(params[:data][:snippets])
+    binding.pry
+    # @cur_snippets = JSON.parse(@highlight.snippets)
+    # @new_snippet = JSON.parse(params[:data][:snippets])
+    # user_id = current_user.id
+    # snippets = JSON.parse(params[:data][:snippets])
 
-    highlight = Highlight.find_by(user_id: user_id, speech_id: speech_id)
+    # highlight = Highlight.find_by(user_id: @selected_user_id, speech_id: @speech_id)
+    @snippets = JSON.parse(@highlight.snippets) || []
 
-    highlight.update(snippets: JSON.parse(highlight.snippets).push(snippets).sort {|a,b| a[1] <=> b[1]})
+    @highlight.update(merge_overlapping_collection(merge_collection(@snippets, @snippet)))
 
 
 
-    @highlights = highlight
-    @speech = Speech.find(speech_id)
-    @selected_user = current_user
+    # highlight.update(snippets: JSON.parse(highlight.snippets).push(@snippet).sort {|a,b| a[1] <=> b[1]})
 
 
     respond_to do |format|
@@ -87,12 +68,10 @@ class HighlightsController < ApplicationController
   # DELETE /highlights/1
   # DELETE /highlights/1.json
   def destroy
-    speech_id = params[:data][:speech_id].to_i
     highlight_id = params[:id].to_i
     user_id = current_user.id
 
-    Highlight.find_by(speech_id: speech_id, user_id: user_id).destroy
-    @speech = Speech.find(speech_id)
+    Highlight.find_by(speech_id: @speech_id, user_id: user_id).destroy
 
     respond_to do |format|
       format.js {}
@@ -108,9 +87,16 @@ class HighlightsController < ApplicationController
     # end
 
     def set_user
-      #note the use of a default value since user might
-      @selected_user_id = params[:data][:user_id].to_i
-      @selected_user = User.find(@selected_user_id)
+      #note the use of a default value
+      # calling to_i on nil, results in 0, so ||= operator won't work as desired
+      if params[:data][:user_id].nil?
+        @selected_user_id = current_user.id
+        @selected_user = current_user
+      else
+        @selected_user_id = params[:data][:user_id].to_i
+        @selected_user = User.find(@selected_user_id)
+      end
+
     end
 
     def set_speech
@@ -119,10 +105,21 @@ class HighlightsController < ApplicationController
     end
 
     def set_highlight
-      # @highlight = JSON.parse(params[:data][:snippets])
-      # @snippet = JSON.parse(params[:data][:snippets])
+      # highlight object
       @highlight = Highlight.find_by(user_id: @selected_user_id, speech_id: @speech_id)
     end
+
+    def set_snippet
+      @snippet = JSON.parse(params[:data][:snippets])
+    end
+
+    # def set_snippets
+    #   binding.pry
+    #   # current snippets in an nested array value (not string)
+    #   @cur_snippets = JSON.parse(@highlight.snippets) || []
+    #   # current snippets in an array value (not string)
+    #   @new_snippet = JSON.parse(params[:data][:snippets])
+    # end
 
     # def check_highlight
     #   @highlight = Highlight.find_by(user_id: current_user.id, speech_id: params[:data][:speech_id].to_i)
--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -3,8 +3,36 @@ class Highlight < ApplicationRecord
   belongs_to :user
 
 
+  def check_indicie_order(e)
+    e[1] > e[0] ? [e[0],e[1]] : [e[1],e[0]]
+  end
 
-  # def check_duplicates(new_snippets)
-  # end
+  # merge_cur_snippets_new_snippets
+  def merge_collection(collection, element)
+    collection.concat(element)
+  end
+
+  def collection_overlap?(l, e)
+    e[0].between?(l.first, l.last) || e[1].between?(l.first, l.last)
+  end
+
+  def merge_overlaps(l, e)
+    [[l.first, e.first].min, [l.last, e.last].max]
+  end
+
+  def merge_overlapping_collection(collection)
+    collection.sort_by(&:first).inject([]) do |collection, element|
+      if !collection.empty? && collection_overlap?(collection.last, element)
+        new_element = merge_overlaps(collection.last, element)
+        new_element != collection.last ? collection.push(new_element) : collection
+      else
+        collection.push(element)
+      end
+    end
+  end
+
+
+
+
 
 end
