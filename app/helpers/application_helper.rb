99ca80390f73a74abee7c9929ad6d8845d977104
lgants <lgants@gmail.com>
2017-02-06 20:54:26 -0600

    implmeneted styling for notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,23 @@
 module ApplicationHelper
+  BOOTSTRAP_FLASH_MSG = {
+      success: 'alert alert-success',
+      error: 'alert alert-danger',
+      alert: 'alert alert-warning',
+      notice: 'alert alert-success'
+    }
 
-  def flash_class(level)
-    case level
-        when :notice then "alert alert-info"
-        when :success then "alert alert-success"
-        when :error then "alert alert-error"
-        when :alert then "alert alert-error"
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym)
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+        concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+        concat message
+      end)
     end
+    nil
   end
 
 end
